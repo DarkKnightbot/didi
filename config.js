6b186e901a29c47b94d7876fab1f1e5b3bdfae3c
Ogunyomi Ayomide <143485093+DarkKnightbot@users.noreply.github.com>
2024-10-04 18:56:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -53,10 +53,10 @@ global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,234xxxxxxxx";
 
 
 //========================= [ AUTO SAVE & READ STATUS ] =========================\\
-global.read_status = process.env.AUTO_READ_STATUS || "true"
+global.read_status = process.env.AUTO_READ_STATUS || "false"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
 global.save_status_from =  process.env.SAVE_STATUS_FROM  || "2348134573414,2349032344526";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "null";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "2348134573414,2349032344526";
 
 global.api_smd = "https://api-smd.onrender.com" //  || "https://api-smd-1.vercel.app" // expires
 global.scan = "https://suhail-md-vtsf.onrender.com";
